e42a5291b70aca41cb5a7bfaefc779f0154514c9
juliendelort <julien.delort@gmail.com>
2022-02-03 10:46:21 -0500

    initial commit

---
+++ src/inform-el.svelte
@@ -0,0 +1,23 @@
+<script>
+    let form;
+    let container;
+
+    function getFormValues() {
+        return Object.fromEntries(new FormData(form));
+    }
+    const handleSubmit = () => {
+        container.dispatchEvent(new CustomEvent("submit", { detail: { values: getFormValues() }, bubbles: true }));
+    };
+</script>
+
+<div bind:this={container}>
+    <form novalidate on:submit|preventDefault|stopPropagation={handleSubmit} bind:this={form}>
+        <slot />
+    </form>
+</div>
+
+<style>
+    form {
+        background: red;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Informel from './inform-el.svelte';
+
+
+import component from 'svelte-tag';
+new component({ component: Informel, tagname: 'form-el', shadow: false, attributes: ['name'] });;
+// var app = new App({
+// 	target: document.body
+// });
+
+// export default app;
