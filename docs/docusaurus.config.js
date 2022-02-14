07bdfbb66ed68e2eb274a76fd1e5baeea07dc9c7
juliendelort <julien.delort@gmail.com>
2022-02-13 19:16:54 -0500

    Docs WIP

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'informel',
-  tagline: 'Form management made easy',
+  tagline: 'Forms with superpowers',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
