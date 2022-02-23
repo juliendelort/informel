4d2f56b047d4b3839ed2b51c52895faa97f2374a
juliendelort <julien.delort@gmail.com>
2022-02-22 19:52:59 -0500

    Updated favicon + front page + more docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -38,6 +38,7 @@ const config = {
       }),
     ],
   ],
+  themes: ['@docusaurus/theme-live-codeblock'],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
--- docs/src/components/HomepageFeatures.js
+++ docs/src/components/HomepageFeatures.js
@@ -4,38 +4,35 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Lightweight',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        ~15kb
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Zero runtime dependency',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Check out package.json!
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Built on top of native features',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        informel doesn't reimplement forms, it leverages native form features to enhance them.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
