abf5b152431f156723c1c9bc70f0ca4dddce9df9
juliendelort <julien.delort@gmail.com>
2023-02-09 10:18:48 -0500

    Redesigned docs site

--- docs/src/components/GetStartedHeader.js
+++ docs/src/components/GetStartedHeader.js
@@ -3,7 +3,7 @@ import Logo from '@site/static/img/logo.svg';
 
 
 export const GetStartedHeader = () => (
-    <h1 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Balsamiq sans' }}>
+    <h1 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Poppins' }}>
         <Logo width="10%" height="10%" />
         <span style={{ marginLeft: '20px', paddingTop: '5px' }}>informel</span>
     </h1>
