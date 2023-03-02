18e6e13859838395457128a9ff4dda3f83f2043f
juliendelort <julien.delort@gmail.com>
2023-03-02 16:37:14 -0500

    Removed comment

--- react-src/index.d.ts
+++ react-src/index.d.ts
@@ -2,8 +2,6 @@ import { ZodType } from 'zod';
 
 type ArrayItemType<T, K = unknown> = T extends Array<infer K> ? K : never;
 
-//https://www.typescriptlang.org/play?#code/C4TwDgpgBAKlC8UDeAoKUCuBnCAnLAXFAIK64CGIAPKuugHbkC2ERtd6AZgJb7BFZgubvQDmAbjQcoAG3KCBQkRKnoAvgBpVUMOVwR6wQqQrV2HRiyIAjAPa2ZEcvUkc1APm3lRrKPQxM1nhSHlroWLYswAAWyorCYq5QnBj0AMZEABQAlAjuUAButtwAJpJqKCigkCRklACSwBBMMOAQVDAaUADSCJj0ANb0tgDu9PmIcBAAHk30JVi1plQinHg9+QD8PVBE9BAFeJKV1dAAYnLA3RAgWB1QM3MLUABKEGm2uCVUggmiXakhqNxl1eogBjdbJxYBMpL1HgZnr9lFBNlIYABtboAXQes0RizOqTSwG4tnoUAAPlAsCBAg5UVJ9odcFIiJicXinosTJQqIDhmMtlJeSBGs1WpAOljsfkEfNFm8Pl8fkoxADBoLxqioAADAAkSG6agxhv8gTwamxADpDRdyFcbndReKWm1pTj3O41LrdnrDcbTUhzUFcFbdWzYDKuQTXu9Pt9ker+kChTqDUa1LakPbHbcPbKfX7upHmcEqm1YBR6FhOJ8mPd5c8lQnVX8NanxhNkKp9OQSuSZCAoFioCIoLnrvmYO5sZsCEmVFbjqcoAApDB9CEgKFxrAYGTAY4nStvfeHvowau1+sdTwVmoAZSUJKoZ27SBHvXH293Z2xRDwviCpQL+0JnDqZzRnsBzrBUD7nMSfQ5PA+Sfn2A70EOI7bvEygAX4sG4FAahQAAZHuB5HpUHw1sAyQEES6TIdkqGZOwADk2B4FgGIAIw2pYEDWjwfAcQQHFNIIHEoGouTiEAA
-
 type FlatKeys<T extends Record<string, unknown>, K = keyof T> =
     K extends string ?
     T[K] extends Function | symbol ?
